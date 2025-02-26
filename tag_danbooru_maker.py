e4a0e8fcd8ae5428a337df258848acbb014cdea4
IAmNamNguyen <138494161+JuniorDevNam@users.noreply.github.com>
2025-02-27 00:13:57 +0700

    ,

--- tag_danbooru_maker.py
+++ tag_danbooru_maker.py
@@ -3,14 +3,24 @@ result = string.replace("absurdres ","")
 result = result.replace("highres ","")
 result = result.replace("watermark ","")
 result = result.replace("artist_name ","")
+result = result.replace("md5_mismatch ","")
+
+result = result.replace("translation_request ","")
+result = result.replace("translated ","")
 
 result = result.replace("chinese_commentary ","")
 result = result.replace("english_commentary ","")
 result = result.replace("commentary_request ","")
 result = result.replace("commentary ","")
 
-result = result.replace("translated ","")
-result = result.replace("speech bubble ","")
+
+result = result.replace("paid_reward_available ","")
+
+result = result.replace("large_variant_set ","")
+result = result.replace("variant_set ","")
+
+result = result.replace("sound_effects ","")
+result = result.replace("speech_bubble ","")
 
 result = result.replace(" ",", ").replace("_"," ").replace("(","\(").replace(")","\)")
 
